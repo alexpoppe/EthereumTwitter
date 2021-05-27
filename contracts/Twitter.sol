280f30c2a6c6ce0a74f289f281dc53b2dc617a30
alexpoppe <alexander.poppe@gmail.com>
2021-05-28 01:20:03 +0200

    twitter with text and Ganache account

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -2,4 +2,25 @@ pragma solidity ^0.5.0;
 
 contract Twitter {
     uint public tweetCount = 0;
+
+    struct Tweet {
+        uint id;
+        string content;
+        string account;
+    }
+
+    mapping(uint => Tweet) public tweets;
+
+    constructor() public {
+        createTweet("my first Tweet");
+    }
+
+    function createTweet(string memory _content, string memory account) public {
+        tweetCount ++;
+        tweets[tweetCount] = Tweet(tweetCount, _content, "MyAccount");
+    }
+
+
+
+
 }
